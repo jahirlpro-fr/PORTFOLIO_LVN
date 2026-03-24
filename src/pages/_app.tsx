5e0c2db18047564aea59f48857d532e31bc07e3e
softgenai <admin@softgen.ai>
2026-03-24 09:18:49 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
